eeeef9c3d8e9c2fec3999d75a42889a05b1d5af1
Amirreza Jabbari <amirrezajabbari79@gmail.com>
2025-05-12 20:49:11 +0330

    init: Upload project

--- setup.py
+++ setup.py
@@ -1,46 +1,31 @@
 from setuptools import setup, find_packages
-import pathlib
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="django-nginx-generator",
+    name="django_nginx_generator",
     version="1.0.0",
-    description="CLI tool to auto-generate production-grade Nginx configs for Django",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Amirreza-Jabbari/django-nginx-generator",
-    author="Amirreza Jabbari",
-    author_email="amirrezajabbari79@gmail.com",
-    license="MIT",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Topic :: System :: Systems Administration",
-    ],
-    py_modules=["generate_nginx"],
+    packages=find_packages(),
     install_requires=[
         "click>=8.0",
         "jinja2>=3.0",
         "Django>=3.2",
     ],
-    entry_points={
-        "console_scripts": [
-            "generate_nginx=generate_nginx:main",
-        ],
-    },
-    include_package_data=True,
+    author="Amirreza Jabbari",
+    description="CLI tool to auto-generate production-grade Nginx configs for Django",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/youruser/django-nginx-generator",
+    author_email="amirrezajabbari79@gmail.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Systems Administration",
+    ],
     project_urls={
-        "Bug Tracker": "https://github.com/Amirreza-Jabbari/django-nginx-generator/issues",
-        "Source Code": "https://github.com/Amirreza-Jabbari/django-nginx-generator",
-        "Documentation": "https://github.com/Amirreza-Jabbari/django-nginx-generator#readme",
+        "Bug Tracker": "https://github.com/amirreza-jabbari/django-nginx-generator/issues",
+        "Source Code": "https://github.com/amirreza-jabbari/django-nginx-generator",
+        "Documentation": "https://github.com/amirreza-jabbari/django-nginx-generator#readme",
     },
 )
